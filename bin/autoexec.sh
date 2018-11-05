2f5c363a6cd7ae284c045346425ba5d952c43aba
Jason Bittel <jason.bittel@urbanairship.com>
2018-11-05 11:50:10 -0800

    Set initial volume level

--- bin/autoexec.sh
+++ bin/autoexec.sh
@@ -12,5 +12,8 @@ xset dpms 300 600 900 &
 # Automatically lock screen
 xautolock -time 10 -detectsleep -locker "~/.dotfiles/bin/i3exit.sh lock" &
 
+# Set initial volume level
+amixer -q sset Master -- -31.0dB &
+
 # Initialize keyring
 /usr/bin/gnome-keyring-daemon --replace --daemonize --components=secrets,ssh
