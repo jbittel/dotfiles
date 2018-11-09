f0d44199d145677730e54fe3272565c2a22cad7b
Jason Bittel <jason.bittel@urbanairship.com>
2018-11-09 09:41:36 -0800

    Refactor terminal script

--- bin/terminal.sh
+++ bin/terminal.sh
@@ -1,20 +1,28 @@
-#!/bin/sh
+#!/usr/bin/env bash
+#
+# Launches a terminal window with a unique instance name to provide
+# a mnemonic device for keeping track of it.
+#
+set -o errexit
+set -o nounset
+set -o pipefail
 
-EMOTIONS_LIST="$HOME/.dotfiles/etc/word-list-emotions.txt"
-NOUNS_LIST="$HOME/.dotfiles/etc/word-list-nouns.txt"
+readonly EMOTIONS_LIST="${HOME}/.dotfiles/etc/word-list-emotions.txt"
+readonly NOUNS_LIST="${HOME}/.dotfiles/etc/word-list-nouns.txt"
 
+# Get a random lowercase word from the provided wordlist
 _get_word() {
-	# Get a random lowercase word from the provided wordlist
-    local word="$(shuf -n1 $1 | tr "[:upper:]" "[:lower:]")"
-    echo "$word"
+    local wordlist="${1:-}"
+    local word="$(shuf -n1 "${wordlist}" | tr "[:upper:]" "[:lower:]")"
+    echo "${word}"
 }
 
+# Generate a random two digit zero-padded number
 _get_number() {
-    # Generate a random two digit zero padded number
-    local number=$(seq -w 0 99 | shuf -n 1)
-    echo "$number"
+    local number="$(seq -w 0 99 | shuf -n 1)"
+    echo "${number}"
 }
 
-INSTANCE_NAME="$(_get_word $EMOTIONS_LIST)-$(_get_word $NOUNS_LIST)"
+INSTANCE_NAME="$(_get_word ${EMOTIONS_LIST})-$(_get_word ${NOUNS_LIST})"
 
-termite --name=$INSTANCE_NAME
+termite --name="${INSTANCE_NAME}"
