2e03900489678e470bce42fe55b94958647e939f
Jason Bittel <jason.bittel@urbanairship.com>
2018-11-08 17:05:47 -0800

    Generate random number for each terminal instance

--- bin/terminal.sh
+++ bin/terminal.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 
+DICTIONARY_PATH="/usr/share/dict/words"
+
 instance_name() {
     # Generate a random string, consisting of two lowercase dictionary
     # words joined by a comma, e.g. "detentions-northerly"
-    local WORDS="$(shuf -n2 /usr/share/dict/words | sed "s/'s//" | tr "[:upper:]" "[:lower:]" | paste -sd "-")"
+    local WORDS=$(shuf -n2 $DICTIONARY_PATH | sed "s/'s//" | tr "[:upper:]" "[:lower:]" | paste -sd "-")
     echo "$WORDS"
 }
 
-termite --name=$(instance_name)
+instance_number() {
+    # Generate a random two digit zero padded number
+    local NUMBER=$(seq -w 0 99 | shuf -n 1)
+    echo "$NUMBER"
+}
+
+termite --name="$(instance_name):$(instance_number)"
